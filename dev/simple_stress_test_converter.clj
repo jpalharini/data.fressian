a8eb1470013cc94f41b2b2b8bbabf8cb52d71426
Joao Palharini <joao@palharini.me>
2025-05-13 10:28:01 -0300

    Use LazilyPersistentVector/createOwning instead of (vec)
    (vec) is a polymorphic method

--- dev/simple_stress_test_converter.clj
+++ dev/simple_stress_test_converter.clj
@@ -1,21 +1,21 @@
 (ns simple-stress-test-converter
   (:require [aux :as a]
-            [clojure.edn :as edn]
-            [metrics :as m])
-  (:import [org.fressian.handlers ConvertList ILookup]))
+            [clojure.edn :as edn])
+  (:import [clojure.lang LazilyPersistentVector]
+           [org.fressian.handlers ConvertList ILookup])
+  (:gen-class))
+
+(def list-converter (reify ConvertList
+                      (convertList [_ items]
+                        (LazilyPersistentVector/createOwning items))))
 
 (def custom-read-handlers
   (reify ILookup
     (valAt [_ k]
-      (get {"fressian/list"
-            (reify ConvertList
-              (convertList [_ items]
-                (vec items)))}
-           k))))
+      (get {"fressian/list" list-converter} k))))
 
 (defn -main [& args]
   (let [parsed-args (map edn/read-string args)
         argmap      (zipmap [:struct-type :struct-size :convert-list?] parsed-args)]
-    (m/initialize-and-return-metrics)
     (a/stress-test argmap custom-read-handlers)
     (System/exit 0)))
