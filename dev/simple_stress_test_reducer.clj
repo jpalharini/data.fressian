d6fa283f5cfc4c8e114748a31d9ec490fa69f5eb
Joao Palharini <joao@palharini.me>
2025-05-13 10:32:04 -0300

    Remove Prometheus metrics

--- dev/simple_stress_test_reducer.clj
+++ dev/simple_stress_test_reducer.clj
@@ -1,11 +1,8 @@
 (ns simple-stress-test-reducer
   (:require [aux :as a]
-            [clojure.core.async :as async]
-            [clojure.edn :as edn]
-            [metrics :as m])
-  (:import [java.io ByteArrayInputStream ByteArrayOutputStream]
-           [org.fressian FressianReader FressianWriter]
-           [org.fressian.handlers ILookup IReduceList]))
+            [clojure.edn :as edn])
+  (:import [org.fressian.handlers ILookup IReduceList])
+  (:gen-class))
 
 (def custom-read-handlers
   (reify ILookup
@@ -24,6 +21,5 @@
 (defn -main [& args]
   (let [parsed-args (map edn/read-string args)
         argmap      (zipmap [:struct-type :struct-size :convert-list?] parsed-args)]
-    (m/initialize-and-return-metrics)
     (a/stress-test argmap custom-read-handlers)
     (System/exit 0)))
